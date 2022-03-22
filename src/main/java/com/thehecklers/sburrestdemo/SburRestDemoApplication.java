b4ddb9382ba57e5558a33e181e0d76e941057443
luan1221 <lssa@cin.ufpe.br>
2022-03-21 21:53:28 -0300

    add configuration properties

--- src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -1,7 +1,10 @@
 package com.thehecklers.sburrestdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SburRestDemoApplication {
 
 	public static void main(String[] args) {
@@ -117,4 +121,52 @@ class RestApiDemoController {
 		coffeeRepository.deleteById(id);
 	}
 
+}
+
+@ConfigurationProperties(prefix = "greeting")
+class Greeting {
+
+	private String name;
+	private String coffee;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCoffee() {
+		return coffee;
+	}
+
+	public void setCoffee(String coffee) {
+		this.coffee = coffee;
+	}
+
+
+
+
+}
+
+@RestController
+@RequestMapping("/greeting")
+class GreetingController {
+
+	private final Greeting greeting;
+
+	public GreetingController(Greeting greeting) {
+		this.greeting = greeting;
+	}
+
+	@GetMapping
+	String getGreeting() {
+		return greeting.getName();
+	}
+
+	@GetMapping("/coffee")
+	String getNameAndCoffee() {
+		return greeting.getCoffee();
+	}
 }
